d16f488bca975249bce36127dee57f22f70f6e21
Linda <isell.linda@gmail.com>
2019-11-14 14:13:00 +0100

    step 7 done

--- src/7-is-empty.js
+++ src/7-is-empty.js
@@ -1,9 +1,14 @@
 export const isEmpty = (stringArrayOrObject) => {
-  const type = typeof stringArrayOrObject
+    const type = typeof stringArrayOrObject
 
-  if (type === 'string') {
-    return stringArrayOrObject === ''
-  }
-
-  return false
+    if (type === 'string') {
+        return stringArrayOrObject === ''
+    }
+    if (type === 'array') {
+        return stringArrayOrObject.length === 0
+    }
+    if (type === 'object') {
+        return Object.entries(stringArrayOrObject).length === 0
+    }
+    return false
 }
